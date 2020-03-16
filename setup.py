b94b9c865c4816571b2567954cba09d53e4924d9
marios8543 <marios8543@gmail.com>
2020-03-16 22:00:58 +0200

    fixed packaging

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/marios8543/aio-mpv-jsonipc",
     packages=setuptools.find_packages(),
-    py_modules=['aio_mpv_jsonipc'],
+    py_modules=['aio_mpv_jsonipc.MPV'],
     classifiers=[
         "Programming Language :: Python :: 3",
          "License :: OSI Approved :: MIT License",
